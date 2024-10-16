e64928a2a375298a02e4e4022fad03a4eb6d6449
David <david.condori@kurax.dev>
2024-10-16 17:25:10 -0500

    [ADD]: starting mini guides

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+      wrap: true,
+    },
+  },
+});
